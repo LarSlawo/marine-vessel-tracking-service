f0eee7bd346108ee4c7e841bc4dfe8816cdbe180
slarysz <slarysz@kamsoft.pl>
2021-11-27 15:36:27 +0100

    Initial commit

---
+++ src/main/java/pl/lartech/mvtservice/MarineVesselTrackingServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.lartech.mvtservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarineVesselTrackingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarineVesselTrackingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/lartech/mvtservice/MarineVesselTrackingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lartech.mvtservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarineVesselTrackingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
